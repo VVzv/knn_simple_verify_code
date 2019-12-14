88eb0f59c194653723cd472fe1f6ee895174c5f6
VVzv <42025843+VVzv@users.noreply.github.com>
2019-12-14 18:08:53 +0800

    Add files via upload

---
+++ knn_accuracy_rate_old.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# __Author__: VVzv
+
+import os
+import joblib
+import numpy as np
+
+from PIL import Image
+# from sklearn.externals import joblib
+
+
+def splitLetter(path):
+    img = np.asarray(Image.open(path).convert('L'))
+    img_binary = (img > 135) * 255
+    img_letters = [
+        img_binary[:, 3: 17],
+        img_binary[:, 17: 31],
+        img_binary[:, 30: 44],
+        img_binary[:, 46: 60]
+    ]
+    letters = []
+    for part in img_letters:
+        letters.append(part.reshape(24*14))
+
+    return letters
+
+def verify(file_name):
+    ver_knn = joblib.load('./models/train_01.model')
+    letters = splitLetter(file_name)
+    pre = ver_knn.predict(letters)
+    code_letter = ''
+    for p in pre:
+        code_letter += p
+    return code_letter
+
+def accuracyRate():
+    path = './test_data/'
+    imgs_name = os.listdir(path)
+    right = []
+    for img_name in imgs_name:
+        code_pre = verify(path + img_name)
+        # print('%s -> %s' % (img_name, verify(path + img_name)))
+        if code_pre == img_name.split('.')[0].upper():
+            right.append(img_name)
+            print('\033[35m[+] %s -> %s\033[0m' %(img_name, code_pre))
+        else:
+            print('\033[31m[-] %s -> %s\033[0m' % (img_name, code_pre))
+    rate = '{:.2f}%'.format(len(right) / len(imgs_name) * 100)
+    return '识别率为：%s' %rate
+
+if __name__ == '__main__':
+    print(accuracyRate())
