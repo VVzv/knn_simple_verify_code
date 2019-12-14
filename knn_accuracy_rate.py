04fa584dc0f6058ff6b88a9c2d0b9ae6099327ee
VVzv <42025843+VVzv@users.noreply.github.com>
2019-12-14 14:46:28 +0800

    Update knn_accuracy_rate.py

--- knn_accuracy_rate.py
+++ knn_accuracy_rate.py
@@ -10,42 +10,40 @@ from PIL import Image
 # from sklearn.externals import joblib
 
 
-def splitLetter(path):
-    img = np.asarray(Image.open(path).convert('L'))
-    img_binary = (img > 135) * 255
-    img_letters = [img_binary[:, 11: 33],
-                   img_binary[:, 36: 58],
-                   img_binary[:, 62: 84],
-                   img_binary[:, 88: 110]]
-    letters = []
-    for part in img_letters:
-        letters.append(part.reshape(48*22))
+LETTERS = ['0', '7', 'r', 'g', '6', 'q', 'f', 'u', '3', '2', 'b', '4', 'j', '5', '1', 'x', '9', 'e', 's', 'z', 'o', 't', 'w', 'i', 'm', 'k', 'y', 'v', '8', 'p', 'n', 'c', 'd', 'l', 'a', 'h']
+base_path = './train_data/'
+letters_file = os.listdir(base_path)
+if '.DS_Store' in letters_file:
+    letters_file = letters_file[1:]
+letters_path = [ base_path + letter for letter in letters_file]
+for letter_path in letters_path:
+    letter_imgs_name = os.listdir(letter_path)
+    if '.DS_Store' in letter_imgs_name:
+        letter_imgs_name = letter_imgs_name[1:]
 
-    return letters
+def getDataDict():
+    num_letter_dict = {}
+    for letter in LETTERS:
+        letter_img_name = os.listdir(base_path+letter)
+        num_letter_dict.update({letter: letter_img_name})
+    return num_letter_dict
 
-def verify(file_name):
-    ver_knn = joblib.load('./models/train_01.model')
-    letters = splitLetter(file_name)
-    pre = ver_knn.predict(letters)
-    code_letter = ''
-    for p in pre:
-        code_letter += p
-    return code_letter
+def loadDataset():
+    X = []
+    y = []
+    img_letter_dict = getDataDict()
+    for ks,vs in img_letter_dict.items():
+        for v in vs:
+            path = base_path + ks + '/' + v
+            pix = np.asarray(Image.open(path).convert("L"))
+            X.append(pix.reshape(48*22))
+            y.append(ks)
+    return np.asarray(X), np.asarray(y)
 
-def accuracyRate():
-    path = './test_data/'
-    imgs_name = os.listdir(path)
-    right = []
-    for img_name in imgs_name:
-        code_pre = verify(path + img_name)
-        # print('%s -> %s' % (img_name, verify(path + img_name)))
-        if code_pre == img_name.split('.')[0].lower():
-            right.append(img_name)
-            print('\033[35m[+] %s -> %s\033[0m' %(img_name, code_pre))
-        else:
-            print('\033[31m[-] %s -> %s\033[0m' % (img_name, code_pre))
-    rate = '{:.2f}%'.format(len(right) / len(imgs_name) * 100)
-    return '识别率为：%s' %rate
 
 if __name__ == '__main__':
-    print(accuracyRate())
+    X, y = loadDataset()
+    ver_knn = joblib.load('./models/train_01.model')
+    score = ver_knn.score(X, y)
+    print('\033[35m数据集精度为：{:.3f}%\033[0m'.format(score))
+
